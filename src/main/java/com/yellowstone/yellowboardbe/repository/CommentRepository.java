df9fed9dd055290506729e31a95dde7d0378ab31
ssomal62 <wxy890@gmail.com>
2024-06-16 20:27:59 +0900

    Fix: CommentRepository 인터페이스 제네릭 타입 수정

--- src/main/java/com/yellowstone/yellowboardbe/repository/CommentRepository.java
+++ src/main/java/com/yellowstone/yellowboardbe/repository/CommentRepository.java
@@ -1,9 +1,10 @@
 package com.yellowstone.yellowboardbe.repository;
 
+import com.yellowstone.yellowboardbe.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository extends JpaRepository<CommentRepository, Integer> {
+public interface CommentRepository extends JpaRepository<CommentEntity, Integer> {
 
 }

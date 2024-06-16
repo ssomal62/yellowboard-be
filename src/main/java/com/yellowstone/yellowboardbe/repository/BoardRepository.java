126fe9f149e521dacf4455acd20ef84deb1f9ba9
ssomal62 <wxy890@gmail.com>
2024-06-16 20:26:54 +0900

    Fix: BoardRepository 인터페이스 제네릭 타입 수정

--- src/main/java/com/yellowstone/yellowboardbe/repository/BoardRepository.java
+++ src/main/java/com/yellowstone/yellowboardbe/repository/BoardRepository.java
@@ -1,9 +1,10 @@
 package com.yellowstone.yellowboardbe.repository;
 
+import com.yellowstone.yellowboardbe.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardRepository extends JpaRepository<BoardRepository, Integer> {
+public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
 
 }

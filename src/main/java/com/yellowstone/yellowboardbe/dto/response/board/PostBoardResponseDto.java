e5c3120c293d583e2671a748c4adfa0dc5d81323
ssomal62 <wxy890@gmail.com>
2024-06-17 23:40:25 +0900

    Feat: Board 요청 및 응답 DTO 추가

---
+++ src/main/java/com/yellowstone/yellowboardbe/dto/response/board/PostBoardResponseDto.java
@@ -0,0 +1,28 @@
+package com.yellowstone.yellowboardbe.dto.response.board;
+
+
+import com.yellowstone.yellowboardbe.dto.ResponseCode;
+import com.yellowstone.yellowboardbe.dto.ResponseDto;
+import com.yellowstone.yellowboardbe.dto.ResponseMessage;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+public class PostBoardResponseDto extends ResponseDto {
+
+    private PostBoardResponseDto() {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+    }
+
+    public static ResponseEntity<PostBoardResponseDto> success() {
+        PostBoardResponseDto result = new PostBoardResponseDto();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> notExistUser() {
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_USER, ResponseMessage.NOT_EXISTED_USER);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result);
+    }
+
+}

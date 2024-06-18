b78c584aa3e595e091b774b8188277fd1c055769
ssomal62 <wxy890@gmail.com>
2024-06-18 12:11:08 +0900

    Feat: ImageRepository에 findByBoardNumber 메서드 추가

--- src/main/java/com/yellowstone/yellowboardbe/repository/ImageRepository.java
+++ src/main/java/com/yellowstone/yellowboardbe/repository/ImageRepository.java
@@ -3,7 +3,12 @@ package com.yellowstone.yellowboardbe.repository;
 import com.yellowstone.yellowboardbe.entity.ImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface ImageRepository extends JpaRepository<ImageEntity, Integer> {
+
+    List<ImageEntity> findByBoardNumber(Integer boardNumber);
 
 }

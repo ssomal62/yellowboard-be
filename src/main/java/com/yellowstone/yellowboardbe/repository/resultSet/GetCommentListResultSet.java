ca7280c4a58fdadcf0cd573a971f9819dfc811b9
ssomal62 <wxy890@gmail.com>
2024-06-18 16:09:56 +0900

    Feat: 댓글 목록 조회 기능 추가
    - GetCommentListResponseDto: 댓글 목록 응답 DTO 추가
    - GetCommentListResultSet: 댓글 목록 결과 인터페이스 추가

---
+++ src/main/java/com/yellowstone/yellowboardbe/repository/resultSet/GetCommentListResultSet.java
@@ -0,0 +1,8 @@
+package com.yellowstone.yellowboardbe.repository.resultSet;
+
+public interface GetCommentListResultSet {
+    String getNickname();
+    String getProfileImage();
+    String getWriteDatetime();
+    String getContent();
+}

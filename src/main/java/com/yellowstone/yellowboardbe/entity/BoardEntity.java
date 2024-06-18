7a3a82500360148ec5ed58a077eed1361b0d16f8
ssomal62 <wxy890@gmail.com>
2024-06-18 14:11:40 +0900

    Feat: 게시물 좋아요 기능 추가
    - BoardController: 게시물 좋아요 엔드포인트 추가
    - BoardEntity: 좋아요 수 증감 메서드 추가
    - BoardService: 좋아요 메서드 인터페이스 추가
    - BoardServiceImpl: 좋아요 로직 구현
    - PutFavoriteResponseDto: 좋아요 응답 DTO 추가
      - 성공, 존재하지 않는 게시물, 존재하지 않는 사용자 응답 포함

--- src/main/java/com/yellowstone/yellowboardbe/entity/BoardEntity.java
+++ src/main/java/com/yellowstone/yellowboardbe/entity/BoardEntity.java
@@ -46,4 +46,12 @@ public class BoardEntity {
     public void increaseViewCount() {
         this.viewCount++;
     }
+
+    public void increaseFavoriteCount() {
+        this.favoriteCount++;
+    }
+
+    public void decreaseFavoriteCount() {
+        this.favoriteCount--;
+    }
 }

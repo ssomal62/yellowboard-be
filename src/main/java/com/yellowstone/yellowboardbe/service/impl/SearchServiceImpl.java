e80619bb345e5b54a0dff835117baaadc54e0935
ssomal62 <wxy890@gmail.com>
2024-06-20 16:52:45 +0900

    Feat: 게시물 연관 검색어 기능 추가
    - SearchController: 연관 검색어 기능 엔드포인트 추가
    - SearchService: 연관 검색어 기능 메서드 인터페이스 추가
    - SearchServiceImpl: 연관 검색어 기능 로직 구현
    - SearchLogRepository: 연관 검색어 조회 쿼리 추가
      - 특정 검색어와 관련된 상위 15개의 검색어를 조회

--- src/main/java/com/yellowstone/yellowboardbe/service/impl/SearchServiceImpl.java
+++ src/main/java/com/yellowstone/yellowboardbe/service/impl/SearchServiceImpl.java
@@ -2,8 +2,10 @@ package com.yellowstone.yellowboardbe.service.impl;
 
 import com.yellowstone.yellowboardbe.dto.ResponseDto;
 import com.yellowstone.yellowboardbe.dto.response.search.GetPopularListResponseDto;
+import com.yellowstone.yellowboardbe.dto.response.search.GetRelationListResponseDto;
 import com.yellowstone.yellowboardbe.repository.SearchLogRepository;
 import com.yellowstone.yellowboardbe.repository.resultSet.GetPopularListResultSet;
+import com.yellowstone.yellowboardbe.repository.resultSet.GetRelationListResultSet;
 import com.yellowstone.yellowboardbe.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -29,5 +31,19 @@ public class SearchServiceImpl implements SearchService {
             ResponseDto.databaseError();
         }
         return GetPopularListResponseDto.success(resultSets);
+    }
+
+    @Override
+    public ResponseEntity<? super GetRelationListResponseDto> getRelationList(String searchWord) {
+        List<GetRelationListResultSet> resultSets = new ArrayList<>();
+        try {
+
+        resultSets = searchLogRepository.getRelationList(searchWord);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            ResponseDto.databaseError();
+        }
+        return GetRelationListResponseDto.success(resultSets);
     }
 }
